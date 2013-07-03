465696c3098dd51c93ee7ceb56ae9baa8d87e5dd
Rémy HUBSCHER <remy.hubscher@novapost.fr>
2013-07-04 01:03:55 +0200

    Add remove password from list + Add reset password

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,6 @@
-var storage_index = "keypass";
+var STORAGE_INDEX = "keypass";
+var PASSWORD_TIMEOUT = 3000;
+
 var __password = '';
 var service_template = '';
 
@@ -9,20 +11,20 @@ function reset_password() {
 function get_password() {
 	if(__password == ''){
 		__password = prompt('Enter your password');
-		setTimeout(reset_password, 3000);
+		setTimeout(reset_password, PASSWORD_TIMEOUT);
 	}
 	return __password;
 }
 
 function get_list() {
-	l = localStorage[storage_index];
+	l = localStorage[STORAGE_INDEX];
     if(typeof(l) == 'undefined') l = [];
 	else l = JSON.parse(l);
    return l;
 }
 
 function set_list(l) {
-    localStorage[storage_index] = JSON.stringify(l);
+    localStorage[STORAGE_INDEX] = JSON.stringify(l);
 }
 
 function add_to_list(value) {
@@ -38,6 +40,18 @@ function del_from_list(value) {
     if (idx != -1) l.splice(idx, 1);
     set_list(l);
     return l;
+}
+
+function del_key_from_list(name) {
+	l = get_list();
+	for(var x in l) {
+		if(l[x]['name'] == name) {
+			l.splice(x, 1);
+			set_list(l);
+			refresh_list();
+			break;
+		}
+	}
 }
 
 function reset_list() {
@@ -64,6 +78,10 @@ $(document).ready(function(){
 		password = get_password();
 		alert(sjcl.decrypt(password, $(this).attr('data-encoded')));
 	});
-	$('#reset').live('click', reset_list);
+	$('button.remove').live('click', function(){
+		del_key_from_list($(this).attr('data-name'));
+	});
+	$('#reset_list').click(reset_list);
+	$('#reset_password').click(reset_password);
 	refresh_list();
 });
