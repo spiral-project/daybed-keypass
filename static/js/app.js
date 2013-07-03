e67b94db81bcad191c1b031ae7d04eb11738c787
Rémy HUBSCHER <remy.hubscher@novapost.fr>
2013-07-04 01:16:50 +0200

    Add UUID to let the user have two password with the same name.

--- static/js/app.js
+++ static/js/app.js
@@ -4,6 +4,21 @@ var PASSWORD_TIMEOUT = 3000;
 var __password = '';
 var service_template = '';
 
+
+// UUID management
+function s4() {
+  return Math.floor((1 + Math.random()) * 0x10000)
+             .toString(16)
+             .substring(1);
+};
+
+function uuid4() {
+  return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+         s4() + '-' + s4() + s4() + s4();
+}
+
+// Password management
+
 function reset_password() {
 	__password = '';
 }
@@ -15,6 +30,8 @@ function get_password() {
 	}
 	return __password;
 }
+
+// LocalStorage management
 
 function get_list() {
 	l = localStorage[STORAGE_INDEX];
@@ -42,10 +59,10 @@ function del_from_list(value) {
     return l;
 }
 
-function del_key_from_list(name) {
+function del_key_from_list(id) {
 	l = get_list();
 	for(var x in l) {
-		if(l[x]['name'] == name) {
+		if(l[x]['id'] == id) {
 			l.splice(x, 1);
 			set_list(l);
 			refresh_list();
@@ -64,12 +81,16 @@ function refresh_list() {
 }
 
 
+// Event Setup
 $(document).ready(function(){
 	service_template = Handlebars.compile($('#service-template').html());
 
     $('#keypass-form').submit(function(event){
 		password = get_password();
-        data = {name: $('#service-name').val(), password: sjcl.encrypt(password, $('#service-password').val()), timestamp: event.timeStamp};
+        data = {id: uuid4(),
+				name: $('#service-name').val(),
+				password: sjcl.encrypt(password, $('#service-password').val()),
+				timestamp: event.timeStamp};
 		add_to_list(data);
 		refresh_list();
 		return false;
@@ -88,7 +109,7 @@ $(document).ready(function(){
 
 	});
 	$('button.remove').live('click', function(){
-		del_key_from_list($(this).attr('data-name'));
+		del_key_from_list($(this).attr('data-id'));
 	});
 	$('#reset_list').click(reset_list);
 	$('#reset_password').click(reset_password);
